02443f608d3f90dc5adfb852388a7528f08eb231
supragya27 <supragya.chauhan001@gmail.com>
2020-11-29 19:40:37 +0530

    updated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Aux from '../../hoc/Auxiliary'
+import classes from './Layout.css'
 
 function Layout(props) {
     return (
         <Aux>
           <div>Toolbar, SideDrawer, Backdrop</div>  
-          <main>
+          <main className={classes.Content}>
           {props.children}
           </main>
         </Aux>
