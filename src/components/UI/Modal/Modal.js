fce685832b8cf4bb8eaf7dbcdbfb7be9c3eef1eb
supragya27 <supragya.chauhan001@gmail.com>
2020-11-30 12:47:43 +0530

    backdrop update

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import Aux from '../../../hoc/Auxiliary'
+import Backdrop from '../Backdrop/Backdrop'
 import classes from './Modal.css'
 
 function Modal(props) {
     return (
+        <Aux>
+        <Backdrop clicked={props.modalClosed} show={props.show}/>
         <div
         style={{
             transform: props.show ? 'translateY(0)' :'translateY(-100vh)',
             opacity: props.show ? '1' :'0'
         }} 
+        onClick={props.clicked}
         className={classes.Modal}>
             {props.children}
         </div>
+        </Aux>
     )
 }
 
