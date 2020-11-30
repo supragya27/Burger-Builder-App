3cc794e4fa6c7ce122281a5e3ea08a006f236a38
supragya27 <supragya.chauhan001@gmail.com>
2020-11-30 22:30:30 +0530

    added shouldComponentUpdate

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary'
-import Backdrop from '../Backdrop/Backdrop'
-import classes from './Modal.css'
+import classes from './Modal.css';
+import Backdrop from '../Backdrop/Backdrop';
 
-function Modal(props) {
-    return (
-        <Aux>
-        <Backdrop clicked={props.modalClosed} show={props.show}/>
-        <div
-        style={{
-            transform: props.show ? 'translateY(0)' :'translateY(-100vh)',
-            opacity: props.show ? '1' :'0'
-        }} 
-        onClick={props.clicked}
-        className={classes.Modal}>
-            {props.children}
-        </div>
-        </Aux>
-    )
+class Modal extends Component {
+
+    shouldComponentUpdate ( nextProps, nextState ) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
 }
 
-export default Modal
+export default Modal;
